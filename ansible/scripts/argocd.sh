9f423f80fd4a4168d25226c03923733361b736a1
joydeep-roychowdhury_skf <noreply@skf.com>
2024-09-18 20:53:57 +0530

    Fix: pushed changes for argocd

--- ansible/scripts/argocd.sh
+++ ansible/scripts/argocd.sh
@@ -5,4 +5,5 @@ export PASSWORD=$(kubectl -n argocd get secret argocd-initial-admin-secret -o js
 
 argocd account update-password --port-forward --account admin --current-password=$PASSWORD --new-password=$NEW_PASSWORD
 argocd login --port-forward --insecure --username admin --password $NEW_PASSWORD
-argocd repo add https://github.com/defyjoy/spark-kubernetes-minio-data-engineering.git --username admin --password $NEW_PASSWORD
+# argocd repo add https://github.com/defyjoy/spark-kubernetes-minio-data-engineering.git --username admin --password $NEW_PASSWORD
+argocd repo add https://github.com/defyjoy/spark-kubernetes-minio-data-engineering.git
