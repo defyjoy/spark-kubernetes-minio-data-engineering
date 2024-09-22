8eed2d93d626e0c347c2d535db77905de3624b8a
joydeep roychowdhury <joydeepbeyondsky86@gmail.com>
2024-09-22 13:41:50 +0530

    Fix: add ingress ip

--- ansible/scripts/argocd.sh
+++ ansible/scripts/argocd.sh
@@ -1,9 +1,7 @@
-NEW_PASSWORD="Password@123"
 kubens argocd
 kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d | xargs argocd login --port-forward --insecure --username admin --password
 export PASSWORD=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo)
 
 argocd account update-password --port-forward --account admin --current-password=$PASSWORD --new-password=$NEW_PASSWORD
 argocd login --port-forward --insecure --username admin --password $NEW_PASSWORD
-# argocd repo add https://github.com/defyjoy/spark-kubernetes-minio-data-engineering.git --username admin --password $NEW_PASSWORD
-argocd repo add https://github.com/defyjoy/spark-kubernetes-minio-data-engineering.git
+argocd repo add https://github.com/defyjoy/spark-kubernetes-minio-data-engineering.git --port-forward
---
+++ ansible/scripts/docker.sh
@@ -0,0 +1,8 @@
+docker network create \
+  --driver=bridge \
+  --subnet=172.18.0.0/16 \
+  --gateway=172.18.0.1 \
+  -o "com.docker.network.bridge.enable_ip_masquerade"="true" \
+  -o "com.docker.network.driver.mtu"="65535" \
+  kind_network
+  # --ip-range=172.18.5.0/24 \
